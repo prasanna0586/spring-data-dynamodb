de0eb1dc3839ef09c10d2e8935a712ecc3192c09
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2025-11-19 21:33:01 -0800

    QueryRequest with indexName change to fix tests

--- src/test/java/org/socialsignin/spring/data/dynamodb/domain/sample/DocumentMetadataRepositoryImpl.java
+++ src/test/java/org/socialsignin/spring/data/dynamodb/domain/sample/DocumentMetadataRepositoryImpl.java
@@ -3,10 +3,9 @@ package org.socialsignin.spring.data.dynamodb.domain.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.socialsignin.spring.data.dynamodb.core.DynamoDBOperations;
-import software.amazon.awssdk.enhanced.dynamodb.Expression;
 import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
-import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
 
 import java.util.HashMap;
 import java.util.List;
@@ -43,26 +42,21 @@ public class DocumentMetadataRepositoryImpl implements DocumentMetadataRepositor
 
                 CompletableFuture.<List<DocumentMetadata>>supplyAsync(() -> {
 
-                // SDK v2: Build filter expression for GSI query
-                // Query on GSI: memberId (partition key) and documentCategory = category (sort key)
+                // SDK v2: Build low-level QueryRequest for GSI query
+                // Query on GSI: memberId-documentCategory-index with memberId (partition) and documentCategory (sort) keys
                 Map<String, AttributeValue> expressionValues = new HashMap<>();
                 expressionValues.put(":memberId", AttributeValue.builder().n(memberId.toString()).build());
                 expressionValues.put(":category", AttributeValue.builder().n(category.toString()).build());
 
-                Expression filterExpression = Expression.builder()
-                    .expression("memberId = :memberId AND documentCategory = :category")
-                    .expressionValues(expressionValues)
-                    .build();
-
-                QueryEnhancedRequest query = QueryEnhancedRequest.builder()
-                    .queryConditional(software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional.keyEqualTo(
-                        k -> k.partitionValue(memberId)
-                    ))
-                    .filterExpression(filterExpression)
+                QueryRequest queryRequest = QueryRequest.builder()
+                    .tableName(dynamoDBOperations.getOverriddenTableName(DocumentMetadata.class, "DocumentMetadata"))
+                    .indexName("memberId-documentCategory-index")
+                    .keyConditionExpression("memberId = :memberId AND documentCategory = :category")
+                    .expressionAttributeValues(expressionValues)
                     .consistentRead(false)
                     .build();
 
-                PageIterable<DocumentMetadata> results = dynamoDBOperations.query(DocumentMetadata.class, query);
+                PageIterable<DocumentMetadata> results = dynamoDBOperations.query(DocumentMetadata.class, queryRequest);
 
                 // Convert PageIterable to List
                 return StreamSupport.stream(results.items().spliterator(), false)
@@ -91,26 +85,21 @@ public class DocumentMetadataRepositoryImpl implements DocumentMetadataRepositor
 
                 CompletableFuture.<List<DocumentMetadata>>supplyAsync(() -> {
 
-                // SDK v2: Build filter expression for GSI query
-                // Query on GSI: memberId (partition key) and documentSubCategory = subCategory (sort key)
+                // SDK v2: Build low-level QueryRequest for GSI query
+                // Query on GSI: memberId-documentSubCategory-index with memberId (partition) and documentSubCategory (sort) keys
                 Map<String, AttributeValue> expressionValues = new HashMap<>();
                 expressionValues.put(":memberId", AttributeValue.builder().n(memberId.toString()).build());
                 expressionValues.put(":subCategory", AttributeValue.builder().n(subCategory.toString()).build());
 
-                Expression filterExpression = Expression.builder()
-                    .expression("memberId = :memberId AND documentSubCategory = :subCategory")
-                    .expressionValues(expressionValues)
-                    .build();
-
-                QueryEnhancedRequest query = QueryEnhancedRequest.builder()
-                    .queryConditional(software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional.keyEqualTo(
-                        k -> k.partitionValue(memberId)
-                    ))
-                    .filterExpression(filterExpression)
+                QueryRequest queryRequest = QueryRequest.builder()
+                    .tableName(dynamoDBOperations.getOverriddenTableName(DocumentMetadata.class, "DocumentMetadata"))
+                    .indexName("memberId-documentSubCategory-index")
+                    .keyConditionExpression("memberId = :memberId AND documentSubCategory = :subCategory")
+                    .expressionAttributeValues(expressionValues)
                     .consistentRead(false)
                     .build();
 
-                PageIterable<DocumentMetadata> results = dynamoDBOperations.query(DocumentMetadata.class, query);
+                PageIterable<DocumentMetadata> results = dynamoDBOperations.query(DocumentMetadata.class, queryRequest);
 
                 // Convert PageIterable to List
                 return StreamSupport.stream(results.items().spliterator(), false)
