ea8a1609b99d96494b72fca7777fce83e7c0d67d
Prasanna Kumar Ramachandran <prasanna0586@gmail.com>
2025-11-22 16:58:14 -0800

    Fixed scan related test failure

--- src/test/java/org/socialsignin/spring/data/dynamodb/domain/sample/PlaylistRepository.java
+++ src/test/java/org/socialsignin/spring/data/dynamodb/domain/sample/PlaylistRepository.java
@@ -43,6 +43,7 @@ public interface PlaylistRepository extends CrudRepository<Playlist, PlaylistId>
     @EnableScan
     List<Playlist> findByDisplayName(String displayName);
 
+    @EnableScan
     List<Playlist> findByUserNameAndDisplayName(String userName, String displayName);
 
     @EnableScan
